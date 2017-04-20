bb11af8f815549558d0092de011868832a1a53d4
226238 <226238@student.pwr.wroc.pl>
2017-04-20 17:55:30 +0200

    v1.0 sortowanie kopcowe

---
+++ lab6/src/itable.hh
@@ -0,0 +1,8 @@
+
+
+class itable
+{
+virtual void heapify (int *tab, int heap_size, int i)=0;
+virtual void budKopiec(int *tab, int rozmiar)=0;
+virtual void sort(int *tab, int rozmiar)=0;
+};
---
+++ lab6/src/table.hh
@@ -0,0 +1,20 @@
+#include "itable.hh"
+#include "czas.hh"
+
+class table:public itable, public wykonywalny
+{
+	int *tab;
+	int rozmiar;
+	
+	public:
+	table();
+
+	virtual void wykonaj(int rozmiar_problemu);
+	virtual string przedstaw_sie();
+	
+
+ 	void heapify (int *tab, int heap_size, int i);
+    void budKopiec(int *tab, int rozmiar);
+    void sort(int *tab, int rozmiar);
+
+};
